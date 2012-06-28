7cba906f051357531747d88900973013bc2e1149
Eamonn Maguire <eamonnmag@gmail.com>
2012-06-28 12:47:08 +0200

    Annotation tagger base class.

---
+++ src/main/java/org/isatools/webvalley/bioimage/AnnotationTagger.java
@@ -0,0 +1,12 @@
+package org.isatools.webvalley.bioimage;
+
+/**
+ * Created by the ISA team
+ *
+ * @author Eamonn Maguire (eamonnmag@gmail.com)
+ *         <p/>
+ *         Date: 28/06/2012
+ *         Time: 12:46
+ */
+public class AnnotationTagger {
+}
